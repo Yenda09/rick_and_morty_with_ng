cb615e21d2ebbf6abb64382339212c5824f33b7b
yenda09 <yeinsdavid09@gmail.com>
2022-12-29 15:43:02 -0500

    initial routes: home, episodes, location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home/home.component';
+import { EpisodesComponent } from './pages/episodes/episodes/episodes.component';
+import { LocationsComponent } from './pages/locations/locations/locations.component';
+import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },{
+    path: 'home',
+    component: HomeComponent
+  },{
+    path: 'episodes',
+    component: EpisodesComponent
+  },{
+    path: 'locations',
+    component: LocationsComponent
+  },{
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { EpisodesComponent } from './pages/episodes/episodes/episodes.component';
+import { LocationsComponent } from './pages/locations/locations/locations.component';
+import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EpisodesComponent,
+    LocationsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
